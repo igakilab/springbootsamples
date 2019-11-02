9250ee3354163ac534e3d4b62879a72a5cab42e4
Hiroshi Igaki <github@musictale.net>
2019-11-02 10:19:15 +0900

    初期サンプル．gradle bootRun を実行し，localhost:8080/sample でアクセス可能

---
+++ src/main/java/jp/ac/igakilab/springbootsamples/controller/SampleController.java
@@ -0,0 +1,16 @@
+package jp.ac.igakilab.springbootsamples.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+/**
+ * SampleController
+ */
+public class SampleController {
+
+  @RequestMapping("/sample")
+  public String sample() {
+    return "sample";
+  }
+}
