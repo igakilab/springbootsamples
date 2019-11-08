201f416fca7223566158586a98664deffc3fcc33
Hiroshi Igaki <github@musictale.net>
2019-11-08 16:42:32 +0900

    特定のページへのリクエストについて，ベーシック認証をかけない設定

--- src/main/java/jp/ac/igakilab/springbootsamples/BasicConfiguration.java
+++ src/main/java/jp/ac/igakilab/springbootsamples/BasicConfiguration.java
@@ -21,6 +21,7 @@ public class BasicConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().antMatchers("/sample").permitAll();
     http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
   }
 }
