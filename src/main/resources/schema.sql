f2b0bcc079c9081414c9b99405c069b102e175de
Hiroshi Igaki <github@musictale.net>
2020-02-19 16:48:43 +0900

    初期環境

--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE fruits (
-    id IDENTITY
-    ,name TEXT NOT NULL
-    ,num INT NOT NULL
-    ,weight  DOUBLE NOT NULL
-    ,details TEXT
-    ,sent BOOLEAN NOT NULL
-);
--- src/test/java/jp/ac/igakilab/springboot_samples/DemoApplicationTests.java
+++ src/test/java/jp/ac/igakilab/springboot_samples/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package jp.ac.igakilab.springbootsamples;
+package jp.ac.igakilab.springboot_samples;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
