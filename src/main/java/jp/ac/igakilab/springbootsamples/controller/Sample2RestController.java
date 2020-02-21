2b7657d7f0a5ee388531fabe63de9658c0556796
Hiroshi Igaki <github@musictale.net>
2020-02-21 11:10:51 +0900

    sample2-2

--- src/main/java/jp/ac/igakilab/springbootsamples/controller/Sample2RestController.java
+++ src/main/java/jp/ac/igakilab/springbootsamples/controller/Sample2RestController.java
@@ -1,7 +1,9 @@
 package jp.ac.igakilab.springbootsamples.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,4 +25,40 @@ public class Sample2RestController {
     return "Hello sample21!";
   }
 
+  /*
+   * http://localhost:8000/api/sample22/hoge
+   * のように指定すると，"/api/sample22"の後に記述された/で区切られた内容(パスパラメータ)を引数として受け取ることができる．
+   *
+   * @GetMapping("sample22/{param}")はsample22/の後の引数をparamという名前で受け取ることを示している．
+   *
+   * (@PathVariable String param)と書くとString
+   * paramにURLのパスパラメータの値をSpringBootが代入してくれる．
+   *
+   * (@PathVariable("param") String param)のように{param}のパスパラメータ名を明示的に指定しても良い
+   */
+  @GetMapping("sample22/{param}")
+  private String sample22Path1(@PathVariable String param) {
+    return "受け取ったパラメータは" + param + "です";
+  }
+
+  /*
+   * http://localhost:8000/api/sample22/hoge/fuga/ パスパラメータを複数受け取ることもできる
+   */
+  @GetMapping("sample22/{param1}/{param2}")
+  private String sample22Path2(@PathVariable String param1, @PathVariable String param2) {
+    return "受け取ったパラメータは" + param1 + "と" + param2 + "です";
+  }
+
+  /*
+   * http://localhost:8000/api/sample22?param=hoge
+   * のように指定すると，"?"以降のparam=hogeの形式の文字列(クエリパラメータ)を受け取ることができる．
+   *
+   * (@RequestParam("param") String param) とかすると?name=hoge
+   * と指定できる．()を省略すると仮引数がクエリパラメータ名になる
+   */
+  @GetMapping("sample22")
+  private String testQueryParam(@RequestParam String param) {
+    return "受け取ったクエリパラメータは" + param + "dayo";
+
+  }
 }
